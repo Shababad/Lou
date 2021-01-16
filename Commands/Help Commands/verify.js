ebd2e45430b3f65e9c3b6ad53cc44dec56cb4f73
Shababad <abieltxchai@gmail.com>
2021-01-16 11:09:56 +0100

    bug fixes 16012021

--- Commands/Help Commands/verify.js
+++ Commands/Help Commands/verify.js
@@ -20,134 +20,80 @@ module.exports = {
         const PTag = await db.get(`${message.author.id}.tag`)
         const vname = await db.get(`${message.guild.id}.vname`)
 
-        
-        if (vchannel == null) {
-            return;
-        }
-        else {
-            if(message.channel.id !== vchannel) {
-                message.channel.send(`This is not a proper channel to verify, Please go to <#${vchannel}>`)
+        if (!args[0]) {
+            if (vchannel == null) {
+                return;
             }
             else {
-                if (vchannel !== null&&role1 !== null&&role2 !== null) {
-                    if (!message.member.roles.cache.has(role1)) {
-                        const send = await message.channel.send('You are already verified!')
-                        if (message.member.roles.cache.has(role2)) {
-                            return;
-                        } else {
-                            message.member.roles.add(role2)
+                if(message.channel.id !== vchannel) {
+                    message.channel.send(`This is not a proper channel to verify, Please go to <#${vchannel}>`)
+                }
+                else {
+                    if (vchannel !== null&&role1 !== null&&role2 !== null) {
+                        if (!message.member.roles.cache.has(role1)) {
+                            const send = await message.channel.send('You are already verified!')
+                            if (message.member.roles.cache.has(role2)) {
+                                return;
+                            } else {
+                                message.member.roles.add(role2)
+                            }
                         }
-                    }
-                    else {
-                        if (PTag !== null) {
-                            const Player = client2.getPlayer(PTag.toUpperCase()).catch(err => {
+                        else {
+                            if (PTag !== null) {
+                                const Player = client2.getPlayer(PTag.toUpperCase()).catch(err => {
+                                    const filter = m => m.author == message.author;
+                                    const collector = message.channel.createMessageCollector(filter);
+
+                                    collector.on('collect', async m => {
+                                        const Player = client2.getPlayer(args[0].toUpperCase()).catch(err => message.channel.send('Player not found'))
+                                        collector.stop()
+                                        message.channel.send(`Saved you as '**${(await Player).name}**'`).then(collector.stop()).then(db.set(`${message.author.id}.tag`, args[0].toUpperCase()))
+                                        message.member.roles.remove(role1).then(message.member.roles.add(role2))
+                                    })
+                                })
+                                message.channel.send(`Verified you as '**${(await Player).name}**'`);
+                                message.member.roles.remove(role1).then(message.member.roles.add(role2));
+                            } else {
+                                message.channel.send('**What is your Brawl Stars tag?**\nExample: #8P8R8QOLP\nType `cancel` to cancel the verification')
                                 const filter = m => m.author == message.author;
                                 const collector = message.channel.createMessageCollector(filter);
 
                                 collector.on('collect', async m => {
-                                    const Player = client2.getPlayer(args[0].toUpperCase()).catch(err => message.channel.send('Player not found'))
-                                    collector.stop()
-                                    message.channel.send(`Saved you as '**${(await Player).name}**'`).then(collector.stop()).then(db.set(`${message.author.id}.tag`, args[0].toUpperCase()))
-                                    message.member.roles.remove(role1).then(message.member.roles.add(role2))
-                                })
-                            })
-                            message.channel.send(`Verified you as '**${(await Player).name}**'`);
-                            message.member.roles.remove(role1).then(message.member.roles.add(role2));
-                        } else {
-                            message.channel.send('**What is your Brawl Stars tag?**\nExample: #8P8R8QOLP\nType `cancel` to cancel the verification')
-                            const filter = m => m.author == message.author;
-                            const collector = message.channel.createMessageCollector(filter);
+                                    if (m.content == "cancel") {
+                                        collector.stop()
+                                        message.channel.send('Action canceled! No verify role you you :eyes:')
 
-                            collector.on('collect', async m => {
-                                if (m.content == "cancel") {
-                                    collector.stop()
-                                    message.channel.send('Action canceled! No verify role you you :eyes:')
-
-                                } else {
-                                    const Player = client2.getPlayer(m.content.toUpperCase()).catch(err => message.channel.send('Player not found, please try again'))
-                                    collector.stop()
-                                    message.channel.send(`Saved you as '**${(await Player).name}**'`).then(db.set(`${message.author.id}.tag`, m.content.toUpperCase()));
-                                    message.member.roles.remove(role1).then(message.member.roles.add(role2));
-                                    if (vname !== null) {
-                                        const trophies = millify((await Player).trophies, {precision: 1})
-                                        const vnameX = vname.replace(/{bs.name}/gi, (await Player).name).replace(/{bs.trophies}/gi, trophies).replace(/{bs.club}/gi, (await Player).club.name).replace(/{bs.tag}/gi, m.content).replace(/{user}/gi, message.author.username).replace(/{user.tag}/gi, message.author.tag).replace(/{user.discriminator}/gi, message.author.discriminator)
-                                        message.member.setNickname(vnameX)
                                     } else {
-                                        return;
-                                    }
+                                        const Player = client2.getPlayer(m.content.toUpperCase()).catch(err => message.channel.send('Player not found, please try again'))
+                                        collector.stop()
+                                        message.channel.send(`Saved you as '**${(await Player).name}**'`).then(db.set(`${message.author.id}.tag`, m.content.toUpperCase()));
+                                        message.member.roles.remove(role1).then(message.member.roles.add(role2));
+                                        if (vname !== null) {
+                                            const trophies = millify((await Player).trophies, {precision: 1})
+                                            const vnameX = vname.replace(/{bs.name}/gi, (await Player).name).replace(/{bs.trophies}/gi, trophies).replace(/{bs.club}/gi, (await Player).club.name).replace(/{bs.tag}/gi, m.content).replace(/{user}/gi, message.author.username).replace(/{user.tag}/gi, message.author.tag).replace(/{user.discriminator}/gi, message.author.discriminator)
+                                            message.member.setNickname(vnameX)
+                                        } else {
+                                            return;
+                                        }
 
-                                }
-                            })
+                                    }
+                                })
+                            }
                         }
                     }
                 }
             }
-        }
-        /*
-        if (vchannel == null) {
-            return;
-        } else {
-            if (PTag !== null) {
-                message.channel.send(`Verification Success! Verified you as ${PTag}`)
-                if (!message.member.roles.cache.has(role2)) {
-                    message.member.roles.add(role2)
-                } if (message.member.roles.cache.has(role1)) {
-                    message.member.roles.remove(role1)
-                }
+        } else if (args[0]) {
+            if (!message.member.hasPermission('MANAGE_GUILD')) {
+                return;
             } else {
-                if (message.channel.id !== vchannel) {
-                    return;
-                }
-                else {
-                    if (message.member.roles.cache.has(role2)) {
-                        message.channel.send('You are already verified!')   
-                    } else {
-                        message.channel.send('**What is your Brawl Stars tag?**\nPlease send the tag under this line, Ex: **#8R8P8QOLP**\n(Type `cancel` to cancel this action!)')
-                        const filter = m => m.author == message.author;
-                        const collector = message.channel.createMessageCollector(filter);
-                
-                        collector.on('collect', async m => {
-                            if (/#([a-zA-Z0-9]{3,15})/.test(m.content)) {
-                                const tag = m.content.split(' ')
-                                const CapTag = tag[0].toUpperCase()
-                                client2.getPlayer(CapTag).catch(message.channel.send('This is not a valid tag, please try again!'))
-                                message.channel.send('Saved!')
-
-                                db.set(`${message.author.id}.tag`, m.content)
-                                collector.stop()
-                                const Player = client2.getPlayer(message.content).catch(message.channel.send('This is not a valid tag!'))
-                                message.channel.send(`successfully saved you as ${Player.name}`)
-                                collector.stop()
-                                message.member.roles.add(role2).then(
-                                    message.member.roles.remove(role1)
-                                ).catch(console.error)
-                                if (vname == null) {
-                                    return;
-                                } else {
-                                    const vnameX = vname.replace(/{bs.name}/gi, Player.name).replace(/{bs.trophies}/gi, Player.trophies).replace(/{bs.club}/gi, Player.club.name).replace(/{bs.tag}/gi, m.content).replace(/{user}/gi, message.author.username).replace(/{user.tag}/gi, message.author.tag).replace(/{user.discriminator}/gi, message.author.discriminator)
-                                    message.author.setNickname(vnameX)
-                                }
-
-                            } else if (/([a-zA-Z0-9]{3,15})/.test(m.content)) {
-                                const addTag = '#' + m.content;
-                                db.set(`${message.author.id}.tag`, addTag)
-                                const Player = client2.getPlayer(addTag).catch(console.error)
-                                message.channel.send(`successfully saved you as ${Player.name}`)
-                                collector.stop()
-                                message.member.roles.add(role2).then(
-                                    message.member.roles.remove(role1)
-                                ).catch(console.error)
-                            } else if (m.content == 'cancel') {
-                                message.channel.send('Action Canceled!')
-                                collector.stop()
-                            }
-                            else {
-                                message.channel.send('Invalid tag.')
-                            }
-                        });
-                    }
+                if (args[0] == 'disable') {
+                    db.set(`${message.guild.id}.vchannel`, null)
+                    db.set(`${message.guild.id}.vrole1`, null)
+                    db.set(`${message.guild.id}.vrole2`, null)
+                    message.channel.send('Successfully ')
                 }
             }
-        }*/
+        }
     }
 }
